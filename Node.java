d449abb2d41935e5b296e446a48a78fb462f29d8
adr0021 <adr0021@auburn.edu>
2017-03-31 13:33:39 -0500

    Reads config file and stores node information in array

--- Node.java
+++ Node.java
@@ -6,15 +6,20 @@ public class Node {
    private String name;
    private String configFile;
    private String nodes;
-   private Node attachedNodes[];
+   private int attachedNodes[];
    private Node currentNode;
    private int numberNodes, value;
    private Node previousNode;
+   private String machine;
+   private String portNumber;
+   private String location;
+   
    
    public Node(String nodeIn) {
       name = nodeIn;
-      for (int i = 0; i < 4; i++) {
-         attachNewNode(null, i);
+      attachedNodes = new int[10];
+      for (int i = 0; i < 10; i++) {
+         attachNewNode(0, i);
       }
    }
    
@@ -26,11 +31,11 @@ public class Node {
       return name;
    }
    
-   public void attachNewNode(Node newNode, int index) {
+   public void attachNewNode(int newNode, int index) {
       attachedNodes[index] = newNode;
    }
    
-   public Node getAttachedNode(int index) {
+   public int getAttachedNode(int index) {
       return attachedNodes[index];
    }
    
@@ -44,6 +49,9 @@ public class Node {
    }
    
    public static void main(String[] args) {
+      Node allNodes[] = new Node[10];
+      String link;
+      Node current;
       if (args.length < 0) {
          System.out.println("Missing argument: Configuration File needed.");
       }
@@ -53,8 +61,46 @@ public class Node {
          BufferedReader buffer = null;
          try {
             buffer = new BufferedReader(new FileReader(file));
+            int added = 0;
+            int i = 0;
             while ((line = buffer.readLine()) != null) {
-               System.out.println(line);
+               Node currentNode = new Node("");
+               int index = line.indexOf(" ");
+               currentNode.setNodeName(line.substring(0, index));
+               line = line.replace(currentNode.getNodeName() + " ", "");
+               index = line.indexOf(" ");
+               currentNode.machine = line.substring(0, index);
+               line = line.replace(currentNode.machine + " ", "");
+               index = line.indexOf(" ");
+               currentNode.portNumber = line.substring(0, index);
+               line = line.replace(currentNode.portNumber + " ", "");
+               index = line.indexOf("links ");
+               currentNode.location = line.substring(0, index);
+               line = line.replace(currentNode.location + "links ", "");  
+               for(int x = 0; x <= 3; x++) {
+                  if((index = line.indexOf(" ")) > 0) {
+                     String linkNode =  line.substring(0, index);
+                     line = line.replace(linkNode + " ", "");
+                     currentNode.attachNewNode(Integer.parseInt(linkNode), x);  
+                  }
+                  else if(line.compareTo("") == 0) {
+                     break;
+                  }
+                  else {
+                     currentNode.attachNewNode(Integer.parseInt(line), x);   
+                     line = line.replace(line, "");
+                  }
+               }
+               allNodes[i] = currentNode; 
+               i++;
+            }
+            for (int y = 0; y < 4; y++) {
+               System.out.print(allNodes[y].getNodeName() + " ");
+               System.out.print(allNodes[y].machine + " " + allNodes[y].portNumber + " " + allNodes[y].location + " links: ");
+               for(int z = 0; z < 4; z++) {
+                  System.out.print(allNodes[y].attachedNodes[z] + " ");
+               }
+               System.out.println("");
             }
          }
          catch (FileNotFoundException e) {
